b97d9b8ed5fe0b7a03c96af6158e013ca4344984
mohammed <mohammed122222248@gmail.com>
2023-03-21 03:59:56 +0200

    frontend

---
+++ main.js
@@ -0,0 +1,14 @@
+let img = document.querySelector('.phone');
+let container = document.querySelector('.container');
+function changes(x){
+    img.src = x;
+}
+function colors(color){
+
+}
+function changes(x){
+    img.src = x;
+}
+function colors(color){
+    container.style.background = color;
+}
